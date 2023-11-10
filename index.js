1075b79f2e45be0457f145d46c0995482613cd2a
Fauzein Mulya Warman <fauzeinmulyawarman@Fauzeins-MacBook-Pro.local>
2023-11-10 20:36:58 +0700

    add cors

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ var dotenv = require('dotenv')
 const bodyParser = require('body-parser');
 
 dotenv.config()
-
 const app = express()
+
+
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    credentials: true, origin: 'http://localhost:3000'
+}))
 app.use(express.json())
 
 app.use(
