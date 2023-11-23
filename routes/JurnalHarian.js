e0828374de50426e86b5062fb96f66fad78b2d9f
xG4L1L30x <macdizofti032@gmail.com>
2023-11-24 00:51:35 +0700

    Add Create Catatan Instruktur

--- routes/JurnalHarian.js
+++ routes/JurnalHarian.js
@@ -6,12 +6,14 @@ var { verifyToken } = require("../middleware/VerifyToken")
 
 const upload = multer();
 
-const { SISWA, PEMBIMBING } = require("../utils/constants")
+const { SISWA, PEMBIMBING, INSTRUKTUR } = require("../utils/constants")
 
 var createJurnalHarianController = require("../controllers/jurnalHarian/CreateJurnalHarianController")
 var createCatatanPembimbingController = require("../controllers/jurnalHarian/CreateCatatanPembimbingController")
+var createCatatanInstrukturController = require("../controllers/jurnalHarian/CreateCatatanInstrukturController")
 
 router.post("/create", verifyToken, checkUserRole(SISWA), upload.single('foto'), createJurnalHarianController)
 router.post("/catatan/pembimbing/create", verifyToken, checkUserRole(PEMBIMBING), createCatatanPembimbingController)
+router.post("/catatan/instruktur/create", verifyToken, checkUserRole(INSTRUKTUR), createCatatanInstrukturController)
 
 module.exports = router 
