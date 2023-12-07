004ca910c1c7e9b8780866bd7d60e8a4f4fb8ad7
xG4L1L30x <macdizofti032@gmail.com>
2023-12-07 18:35:07 +0700

    Add get absensi siswa

---
+++ controllers/absensi/GetAbsensiSiswaController.js
@@ -0,0 +1,88 @@
+const BaseResponse = require("../../dto/BaseResponse")
+const siswaService = require("../../services/Siswa")
+const kelompokBimbinganService = require("../../services/KelompokBimbingan")
+const absensiService = require("../../services/Absensi")
+
+async function handler(req, res) {
+    const result = new BaseResponse()
+
+    if ( !req.query.bulan || !req.query.tahun ) {
+        result.success = false
+        result.message = "Parameter bulan dan tahun harus diisi..."
+        return res.status(400).json(result)
+    }
+
+    const bulan = req.query.bulan
+    const tahun = req.query.tahun
+
+    var cekSiswa =  await siswaService.findOne({
+        nisn: req.username
+    })
+
+    var id_siswa = cekSiswa.data.id
+
+    var cekKelompokBimbingan = await kelompokBimbinganService.findOne({
+        id_siswa: id_siswa
+    })
+
+    var id_bimbingan = cekKelompokBimbingan.data.id
+
+    var absensi = await absensiService.search(
+        {
+            id_bimbingan,
+            tanggal: {
+                gte: new Date(tahun, bulan - 1, 1),
+                lt: new Date(tahun, bulan, 1)
+            },
+        },
+        {
+            tanggal: 'asc',
+        }
+    )
+
+    if (absensi.success && absensi.data.length > 0) {
+        
+        var jml_kehadiran = {
+            HADIR: 0,
+            LIBUR: 0,
+            SAKIT: 0,
+            ALPA: 0,
+            IZIN: 0,
+        }
+
+        absensi.data.forEach(element => {
+            if (element.status === "HADIR") {
+                jml_kehadiran.HADIR += 1
+            } else if (element.status === "LIBUR") {
+                jml_kehadiran.LIBUR += 1
+            } else if (element.status === "SAKIT") {
+                jml_kehadiran.SAKIT += 1
+            } else if (element.status === "ALPA") {
+                jml_kehadiran.ALPA += 1
+            } else if (element.status === "IZIN") {
+                jml_kehadiran.IZIN += 1
+            }
+        });
+
+        var dataAbsensi = {
+            jml_kehadiran,
+            data_kehadiran: absensi.data
+        }
+
+        result.success = true
+        result.message = "Data absensi berhasil ditampilkan..."
+        result.data = dataAbsensi
+        return res.status(200).json(result)
+    } else if (absensi.success && absensi.data.length === 0) {
+        result.success = true
+        result.message = "Data absensi tidak ditemukan..."
+        result.data = absensi.data
+        return res.status(401).json(result)
+    } else {
+        result.success = false
+        result.message = "Internal Server Error"
+        return res.status(500).json(result)
+    }
+}
+
+module.exports = handler
--- routes/Absensi.js
+++ routes/Absensi.js
@@ -1,10 +1,12 @@
 const router = require("express").Router()
 var { verifyToken } = require("../middleware/VerifyToken")
 var { checkUserRole } = require("../middleware/CekRoles")
-const { PEMBIMBING } = require("../utils/constants")
+const { PEMBIMBING, SISWA } = require("../utils/constants")
 
 var submitAbsensiController = require("../controllers/absensi/SubmitAbsensiController")
+var getAbsensiSiswaController = require("../controllers/absensi/GetAbsensiSiswaController")
 
 router.post("/submit", verifyToken, checkUserRole(PEMBIMBING), submitAbsensiController)
+router.get("/get/siswa", verifyToken, checkUserRole(SISWA), getAbsensiSiswaController)
 
 module.exports = router
