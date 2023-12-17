a6fcb58f069a2dfa2f30b7be631722e600f8c3bb
Fauzein Mulya Warman <fauzeinmulyawarman@Fauzeins-MacBook-Pro.local>
2023-12-17 16:51:04 +0700

    Tes Tracing endpoint delete

--- middleware/VerifyToken.js
+++ middleware/VerifyToken.js
@@ -4,6 +4,13 @@ async function verifyToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1];
 
+    return res.status(401).json({
+        success: false,
+        message: 'TTTTEEEEESSSSSSSS',
+        token: token,
+        authHeader
+    });
+
     if(token == null){
         return res.status(401).json({
             success: false,
