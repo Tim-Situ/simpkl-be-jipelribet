fea49a7d81af87fc7dbf413207b1b9050c94268d
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-04-22 07:43:21 +0700

    adding env example

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const express = require("express")
-var cookieParser = require("cookie-parser")
-var cors = require('cors')
-var dotenv = require('dotenv')
-const bodyParser = require('body-parser');
+const express = require("express");
+var cookieParser = require("cookie-parser");
+var cors = require("cors");
+var dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 
-
-app.use(cookieParser())
-app.use(cors({
+app.use(cookieParser());
+app.use(
+  cors({
     credentials: true,
-    origin: ['http://localhost:3000', 'https://sispensim-prod.haululazkiyaa.id', 'https://sispensim.haululazkiyaa.id'],
-}))
-app.use(express.json())
+    origin: process.env.ORIGIN.split(", "),
+  })
+);
+app.use(express.json());
 
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    }),
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
 // Route
-app.use("/auth", require("./routes/Auth"))
-app.use("/tes", require("./routes/Tes"))
-app.use("/tahun-ajaran", require("./routes/TahunAjaran"))
-app.use("/jurusan", require("./routes/Jurusan"))
-app.use("/guru-pembimbing", require("./routes/GuruPembimbing"))
-app.use("/siswa", require("./routes/Siswa"))
-app.use("/perusahaan", require("./routes/Perusahaan"))
-app.use("/instruktur", require("./routes/Instruktur"))
-app.use("/kelompok-bimbingan", require("./routes/KelompokBimbingan"))
-app.use("/tujuan-pembelajaran", require("./routes/TujuanPembelajaran"))
-app.use("/nilai-bulanan", require("./routes/NilaiBulanan"))
-app.use("/jurnal-harian", require("./routes/JurnalHarian"))
-app.use("/aspek-penilaian", require("./routes/AspekPenilaian"))
-app.use("/absensi", require("./routes/Absensi"))
+app.use("/auth", require("./routes/Auth"));
+app.use("/tes", require("./routes/Tes"));
+app.use("/tahun-ajaran", require("./routes/TahunAjaran"));
+app.use("/jurusan", require("./routes/Jurusan"));
+app.use("/guru-pembimbing", require("./routes/GuruPembimbing"));
+app.use("/siswa", require("./routes/Siswa"));
+app.use("/perusahaan", require("./routes/Perusahaan"));
+app.use("/instruktur", require("./routes/Instruktur"));
+app.use("/kelompok-bimbingan", require("./routes/KelompokBimbingan"));
+app.use("/tujuan-pembelajaran", require("./routes/TujuanPembelajaran"));
+app.use("/nilai-bulanan", require("./routes/NilaiBulanan"));
+app.use("/jurnal-harian", require("./routes/JurnalHarian"));
+app.use("/aspek-penilaian", require("./routes/AspekPenilaian"));
+app.use("/absensi", require("./routes/Absensi"));
 
 app.listen(process.env.APP_PORT, () => {
-    console.log("Server up and running...")
-}) 
-
+  console.log("Server up and running...");
+});
