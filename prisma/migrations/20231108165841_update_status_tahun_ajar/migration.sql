7f8cee85afc8ca1bc334c0a289d2752754b5e07a
fauzein <fauzeinmw2@gmail.com>
2024-11-20 17:45:16 +0700

    seeting ulang

--- prisma/migrations/20231108165841_update_status_tahun_ajar/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `status` on the `tahun_ajaran` table. The data in that column could be lost. The data in that column will be cast from `Text` to `TinyInt`.
-
-*/
--- AlterTable
-ALTER TABLE `tahun_ajaran` MODIFY `status` BOOLEAN NOT NULL DEFAULT false;
